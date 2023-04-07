3b706863ba5792c73f178fde0ccb5d19ff6b4be4
Umar Mohammad <umar.tli.edu@gmail.com>
2023-04-07 15:06:22 -0500

    mysql connection

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello, world!")
+import mysql.connector
+
+connection = mysql.connector.connect(user = "root", database="people", password="thomstarry")
+
+
+cursor = connection.cursor()
+
+cursor.execute("select * from people;")
+
+for item in cursor:
+    print(item)
+
+cursor.close()
+connection.close()
